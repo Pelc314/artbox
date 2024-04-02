4e70a630b1d07b44d1c6276be79c57009d1b27e0
Pelc314 <mpepelc@gmail.com>
2024-04-02 20:34:22 +0200

    added responsivnes to components

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ToastService } from '../services/toast.service';
+import { ResponsiveLayout } from '../services/responsiveLayout.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  subscriptionHandsetPortrait: Subscription = new Subscription;
+  subscriptionHandsetLandscape: Subscription = new Subscription;
+  subscriptionMedium: Subscription = new Subscription;
+  subscriptionLarge: Subscription = new Subscription;
+  isLayoutHandsetPortrait: boolean = false;
+  isLayoutHandsetLandscape = false;
+  isLayoutLarge = false;
+  isLayoutMedium = false;
   mobileNumber = '+48 604 912 792';
   phoneNumber = '(0 71) 345 22 22';
+  email = 'biuro@artbox.com.pl';
 
-  constructor(private toastService: ToastService) { }
+  constructor(private readonly toastService: ToastService, private readonly responsiveLayoutService: ResponsiveLayout) { }
+
+  ngOnInit(): void {
+    this.subscriptionHandsetPortrait = this.responsiveLayoutService.
+      isLayoutHandsetPortait$.subscribe(isLayoutHandsetPortrait => {
+        this.isLayoutHandsetPortrait = isLayoutHandsetPortrait;
+      });
+
+    this.subscriptionHandsetLandscape = this.responsiveLayoutService.
+      isLayoutHandsetLandscape$.subscribe(result => {
+        this.isLayoutHandsetLandscape = result;
+      });
+
+    this.subscriptionMedium = this.responsiveLayoutService.
+      isLayoutMedium$.subscribe(result => {
+        this.isLayoutMedium = result;
+      });
+
+    this.subscriptionLarge = this.responsiveLayoutService.
+      isLayoutLarge$.subscribe(result => {
+        this.isLayoutLarge = result;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionHandsetPortrait.unsubscribe();
+    this.subscriptionHandsetLandscape.unsubscribe();
+    this.subscriptionMedium.unsubscribe();
+    this.subscriptionLarge.unsubscribe();
+  }
 
   handleShowMap(): void {
     const artboxAddress = "/Artbox.+Dudkowska+K."
@@ -19,6 +59,6 @@ export class FooterComponent {
   }
 
   handleClick(): void {
-    this.toastService.show('Skopiowano do schowka','Zamknij');
+    this.toastService.show('Skopiowano do schowka', 'Zamknij');
   }
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ResponsiveLayout } from '../services/responsiveLayout.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-  
+export class HomePageComponent implements OnInit, OnDestroy {
+  subscriptionHandsetPortrait: Subscription = new Subscription;
+  subscriptionHandsetLandscape: Subscription = new Subscription;
+  subscriptionMedium: Subscription = new Subscription;
+  subscriptionLarge: Subscription = new Subscription;
+  isLayoutHandsetPortrait: boolean = false;
+  isLayoutHandsetLandscape = false;
+  isLayoutLarge = false;
+  isLayoutMedium = false;
+
+  constructor(private readonly responsiveLayoutService: ResponsiveLayout) { }
+
+  ngOnInit(): void {
+    this.subscriptionHandsetPortrait = this.responsiveLayoutService.
+      isLayoutHandsetPortait$.subscribe(isLayoutHandsetPortrait => {
+        this.isLayoutHandsetPortrait = isLayoutHandsetPortrait;
+      });
+
+    this.subscriptionHandsetLandscape = this.responsiveLayoutService.
+      isLayoutHandsetLandscape$.subscribe(result => {
+        this.isLayoutHandsetLandscape = result;
+      });
+
+    this.subscriptionMedium = this.responsiveLayoutService.
+      isLayoutMedium$.subscribe(result => {
+        this.isLayoutMedium = result;
+      });
+
+    this.subscriptionLarge = this.responsiveLayoutService.
+      isLayoutLarge$.subscribe(result => {
+        this.isLayoutLarge = result;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionHandsetPortrait.unsubscribe();
+    this.subscriptionHandsetLandscape.unsubscribe();
+    this.subscriptionMedium.unsubscribe();
+    this.subscriptionLarge.unsubscribe();
+  }
+
 }
